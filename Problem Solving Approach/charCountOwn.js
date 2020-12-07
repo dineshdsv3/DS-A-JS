ad409c199c7201760392104684cabcf9eded257e
dineshdsv3 <dineshdsv3@gmail.com>
2020-12-07 22:52:17 +0530

    Added Lowercasing to the charCount

--- Problem Solving Approach/charCountOwn.js
+++ Problem Solving Approach/charCountOwn.js
@@ -1,17 +1,18 @@
 // Write a function which takes in a string and returns count of each character in the string.
 
-function charCount(char) {
+function charCount(str) {
 	let result = {};
 
-	for (let i = 0; i < char.length; i++) {
-		if (result[char[i]] > 0) {
-			result[char[i]]++;
+	for (let i = 0; i < str.length; i++) {
+		let char = str[i].toLowerCase();
+		if (result[char] > 0) {
+			result[char]++;
 		} else {
-			result[char[i]] = 1;
+			result[char] = 1;
 		}
 	}
 	console.log(result);
 }
 
 charCount('abab');
-charCount('Hhello')
+charCount('Hhello');
