c3c728d5434d8744893d41f0bdd9da7608f2c789
dineshdsv3 <dineshdsv3@gmail.com>
2020-12-12 18:59:41 +0530

    POM-diagnolDifference

--- HackerRank/diagnolDifference.js
+++ HackerRank/diagnolDifference.js
@@ -5,6 +5,10 @@
 // 9 8 9
 // The left-to-right diagonal = 1+5+9 => 15. The right to left diagonal 3+5+9=17 . Their absolute difference is |15-17| =>2.
 
+// Points to remember from this Challenge
+// 1) When trying to solve 2d arrays, remember about the 2D indexes = arr[0][0]
+// 2) In the line 16 remember about the logic while dealing with the indexes
+
 function diagnolDifference(arr) {
 	let sum = 0;
 	for (let i = 0; i < arr.length; i++) {
