4b37055e2924130291a59b208926eb245571fe86
dineshdsv3 <dineshdsv3@gmail.com>
2020-12-24 16:22:19 +0530

    Optional Challenge - isSubsequence - Optimal

--- Optional Challenges/isSubsequence.js
+++ Optional Challenges/isSubsequence.js
@@ -6,20 +6,27 @@
 // Sample Output - isSubsequence('abc', 'abracadabra') => true
 // Sample Output - isSubsequence('abc', 'acb') => false
 
+// Points To Remember - Multiple Point Pattern
+// compare the both strings by each character
+// if character matches move to next character in both strings
+// If not matches increment character in 2nd string
+// If length of the str1 completes return true else retrun false
+
 function isSubsequence(str1, str2) {
 	let s1 = 0;
 	let s2 = 0;
-	let count = 0;
+	// let count = 0;
 	while (s1 < str1.length && s2 < str2.length) {
 		if (str1[s1] === str2[s2]) {
 			s1++;
 			s2++;
-			count++;
+			// count++;
+			if (s1 === str1.length) return true; // optimal solution
 		} else {
 			s2++;
 		}
 	}
-	return count === str1.length;
+	return false;
 }
 
 console.log(isSubsequence('hello', 'hello world'));
