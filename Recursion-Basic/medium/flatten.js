7a99afc35f521998ff557c5a4812c26038b921e5
dineshdsv3 <dineshdsv3@gmail.com>
2021-01-05 23:58:42 +0530

    Recursion - flatten.js

---
+++ Recursion-Basic/medium/flatten.js
@@ -0,0 +1,29 @@
+// Write a recursive function called flatten which accepts an array of arrays and
+// returns a new array with all values flattened
+// flatten([1, 2, 3, [4, 5] ]) => [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) => [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) => [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) => [1,2,3
+
+function flatten(arr) {
+	let result = [];
+
+	function helper(input) {
+		if (input.length === 0) return;
+
+		if (!Array.isArray(input[0])) {
+			result.push(input[0]);
+		} else {
+			helper(input[0]);
+		}
+		helper(input.slice(1));
+	}
+
+	helper(arr);
+	return result;
+}
+
+console.log(flatten([1, 2, 3, [4, 5]]));
+console.log(flatten([1, [2, [3, 4], [[5]]]]));
+console.log(flatten([[1], [2], [3]]));
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]));
